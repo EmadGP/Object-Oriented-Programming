aa7fd2c36423d67d183ddeafe18e8e92b55059db
Atamagai <99071577@mydavinci.nl>
2024-03-01 16:39:42 +0100

    Done

--- pokemon_battle_simulator.py
+++ pokemon_battle_simulator.py
@@ -2,10 +2,14 @@ from models import *
 from data import *
 
 def main():
+<<<<<<< Updated upstream
     charmander = Charmander("Charmander")
 
+=======
+>>>>>>> Stashed changes
 
     game_running = True
+    Rematch = True
 
     while game_running:
 
@@ -29,6 +33,7 @@ def main():
             opponent_naam = input("Please choose a name for the challenger\n")
             opponent = Trainer(opponent_naam)
 
+<<<<<<< Updated upstream
     # <--------- The player gives a name to a charmander --------->
             
             name = str(input("Give a name to your Charmander: "))
@@ -53,6 +58,61 @@ def main():
                 str(input('what is the name you would like tho give: '))
             elif changename == 2:
                 game_running = True
+=======
+            print(f"{challenger.name} vs {opponent.name}")
+
+    # <---------- 6 Pokeball's are added to the challenger's belt ---------->
+
+            for i in range(2):
+
+                charmander = Charmander("Charmander")
+                pokeball = Pokeball(charmander)
+                challenger.takePokeball(pokeball)
+
+                squirtle = Squirtle("Squirtle")
+                pokeball = Pokeball(squirtle)
+                challenger.takePokeball(pokeball)
+
+                bulbasaur = Bulbasaur("Bulbasaur")
+                pokeball = Pokeball(bulbasaur)
+                challenger.takePokeball(pokeball)
+
+        # <---------- 6 Pokeball's are added to the opponent's belt ---------->
+
+            for i in range(2):
+
+                charmander = Charmander("Charmander")
+                pokeball = Pokeball(charmander)
+                opponent.takePokeball(pokeball)
+
+                squirtle = Squirtle("Squirtle")
+                pokeball = Pokeball(squirtle)
+                opponent.takePokeball(pokeball)
+
+                bulbasaur = Bulbasaur("Bulbasaur")
+                pokeball = Pokeball(bulbasaur)
+                opponent.takePokeball(pokeball)
+
+                while Rematch:
+
+                    thrown_pokeball_c = challenger.throwPokeball()  # The challenger throws a pokeball
+
+                    thrown_pokeball_o = opponent.throwPokeball()    # The opponent throws a pokeball
+
+                    challenger.returnPokeball(thrown_pokeball_c)    # The challenger returns the pokeball
+
+                    opponent.returnPokeball(thrown_pokeball_o)      # The opponent returns the pokeball
+
+                    rematch = input("Do you want to play again? (yes/no)\n")
+
+                    if rematch == "yes":
+                        Rematch = True
+                    else:
+                        Rematch = False
+                        game_running = False
+                        print("Game over!")
+                        quit()
+>>>>>>> Stashed changes
 
     # <------ The player can choose to quit the game or continue ------>
 
